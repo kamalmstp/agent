f0ad8d74449bd438af4728791df803374cd88326
Kamal <mustapakamalkml@gmail.com>
2025-08-28 16:06:54 +0700

    sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->group('api', [
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            EnsureFrontendRequestsAreStateful::class,
+            SubstituteBindings::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PendudukController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/penduduks', [PendudukController::class, 'index'])->name('penduduks.index');
 
-Route::post('/api/penduduks', [PendudukController::class, 'store'])->middleware('api');
+Route::middleware('api')->group(function () {
+    // Tambahkan rute API lainnya di sini jika diperlukan
+    Route::post('/penduduks', [PendudukController::class, 'store']);
+});
